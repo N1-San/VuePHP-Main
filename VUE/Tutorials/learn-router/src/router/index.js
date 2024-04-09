c1a5887333c6205945d07bc3a7bfdc9e93c68848
Aniki <33519431+N1-San@users.noreply.github.com>
2024-04-09 23:21:47 +0530

    vue tutorial

---
+++ VUE/Tutorials/learn-router/src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView";
+import Jobs from "../views/Jobs/Jobs";
+import JobDetails from "../views/Jobs/JobDetails";
+import NotFound from "@/views/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: AboutView,
+    // () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+  {
+    path:'/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  //redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  //catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ VUE/Tutorials/learn-router/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Home  View</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nostrum, qui necessitatibus illum obcaecati sapiente inventore eligendi aspernatur beatae quo facere dicta corrupti cupiditate fugiat corporis ipsa dolorem assumenda quam. Saepe eius, asperiores voluptatem alias repellat, facilis, sint laboriosam culpa ullam magni architecto! Veritatis deleniti qui hic quia delectus velit minus aliquam consectetur placeat voluptatem minima accusantium facere dignissimos nam obcaecati aliquid, saepe nisi consequatur quam mollitia. Velit consequuntur architecto eum, magnam illo perspiciatis accusantium tenetur ut excepturi doloribus perferendis blanditiis earum sapiente totam porro ipsa quae! Error, dolorum! Repellat cum, consequatur facilis officiis assumenda magnam voluptate recusandae expedita itaque!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nostrum, qui necessitatibus illum obcaecati sapiente inventore eligendi aspernatur beatae quo facere dicta corrupti cupiditate fugiat corporis ipsa dolorem assumenda quam. Saepe eius, asperiores voluptatem alias repellat, facilis, sint laboriosam culpa ullam magni architecto! Veritatis deleniti qui hic quia delectus velit minus aliquam consectetur placeat voluptatem minima accusantium facere dignissimos nam obcaecati aliquid, saepe nisi consequatur quam mollitia. Velit consequuntur architecto eum, magnam illo perspiciatis accusantium tenetur ut excepturi doloribus perferendis blanditiis earum sapiente totam porro ipsa quae! Error, dolorum! Repellat cum, consequatur facilis officiis assumenda magnam voluptate recusandae expedita itaque!</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
