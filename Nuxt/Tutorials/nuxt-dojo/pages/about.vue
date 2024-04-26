90d4720b6320a806973e1ba4c28d0f479c187894
Aniki <33519431+N1-San@users.noreply.github.com>
2024-04-26 11:56:28 +0530

    nuxt

---
+++ Nuxt/Tutorials/nuxt-dojo/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam error delectus nulla id laudantium iste optio veniam, expedita natus nemo.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
---
+++ Nuxt/Tutorials/nuxt-dojo/pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam laudantium vero consequatur doloremque, quo assumenda, veritatis voluptatum sit corrupti amet natus distinctio hic delectus, quia sequi molestiae? Minima, incidunt facere.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+definePageMeta({
+    layout:'products'
+})
+</script>
+
+<style scoped>
+
+</style>
