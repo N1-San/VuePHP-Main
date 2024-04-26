201ab438a987fd1cdd1a1ae3ada2df81db9e637c
Aniki <33519431+N1-San@users.noreply.github.com>
2024-04-26 11:56:39 +0530

    laravel

--- Laravel/example/resources/views/about.blade.php
+++ Laravel/example/resources/views/about.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
-</head>
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-    <h1>The About page</h1>
-</body>
-</html>
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    The about page works
+</x-layout>
--- Laravel/example/resources/views/home.blade.php
+++ Laravel/example/resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <h1>The layout works</h1>
 </x-layout>
